b1f7dc45d5c062bf991f048a0aad029b19e7357d
mtrigueros <moisestrigueros@hotmail.com>
2019-01-27 02:06:42 -0600

    Incorporar componentes de login y register,modulos core y shared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { CoreModule } from "./core/core.module";
+import { AppRoutingModule } from "./app-routing.module";
+import { RegisterComponent } from "./components/register/register.component";
+import { LoginComponent } from "./components/login/login.component";
+import {MDBBootstrapModulesPro, MDBSpinningPreloader} from 'ng-uikit-pro-standard';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent, LoginComponent, RegisterComponent],
+	imports: [BrowserModule, CoreModule, MDBBootstrapModulesPro.forRoot(), AppRoutingModule],
+	schemas: [NO_ERRORS_SCHEMA],
+	providers: [MDBSpinningPreloader],
+	bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
