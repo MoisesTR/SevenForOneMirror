0209952c23b582d8a10a8e29ab2eb36d265316aa
Enmanuel <rogeersann97@gmail.com>
2019-02-10 02:57:26 -0600

    add models card

---
+++ src/app/models/Card.ts
@@ -0,0 +1,15 @@
+export class Card{
+
+    public IdUser: number;
+    public CardNumer: string;
+    public CardType: string;
+    public ExpirationDate: string;
+    public Enable: boolean;
+    public CreatedAt: string;
+    public UpdateAt: string;
+
+    constructor(){
+        
+    }
+
+}
