dd16e7f64f2dae5a0ee87d86e9987903635b5cc1
mtrigueros <moisestrigueros@hotmail.com>
2019-02-10 04:51:58 -0600

    Corregir modelo de usuario

--- src/app/models/User.ts
+++ src/app/models/User.ts
@@ -1,20 +1,20 @@
-export class User{
+export class User {
+	public _id: number;
+	public firstName: string;
+	public lastName: string;
+	public userName: string;
+	public email: string;
+	public phones: string[];
+	public birthDate: string;
+	public gender: string;
+	public secretToken: string;
+	public isVerified: boolean;
+	public role: string;
+	public passwordHash: string;
+	public password: string;
+	public enabled: number;
+	public createdAt: string;
+	public updatedAt: string;
 
-    public FirstName: string;
-    public LastName: string;
-    public UserName: string;
-    public Email: string;
-    public Phone: string;
-    public BirthDate: string;
-    public SecretToken: string;
-    public IsVerified: boolean;
-    public IdRole: number;
-    public Password: string;
-    public Enable: number;
-    public CreatedAt: string;
-    public UpdateAt: string;
-
-    constructor(){
-
-    }
+	constructor() {}
 }
