96603e4ab29f422192020519be087a00e2311723
mtrigueros <moisestrigueros@hotmail.com>
2019-02-09 22:52:42 -0600

    Incorporar menu,dashboard,notfound y menu routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,29 @@
 import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
+import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
 import { LoginComponent } from "./components/login/login.component";
 import { RegisterComponent } from "./components/register/register.component";
+import { MenuComponent } from "./components/menu/menu.component";
+import {NotFound404Component} from './components/not-found404/not-found404.component';
 
 export const routes: Routes = [
 	{ path: "login", component: LoginComponent },
 	{ path: "register", component: RegisterComponent },
-	{ path: "", component: LoginComponent }
+	{
+		path: "",
+		component: MenuComponent,
+		loadChildren: "./components/menu/menu.module#MenuModule",
+		data: { titulo: "Menu" }
+	},
+  {
+    path: "**",
+    component: NotFound404Component,
+    data: {
+      titulo: "No encontrado"
+    }
+  }
 ];
 @NgModule({
-	imports: [RouterModule.forRoot(routes)],
+	imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
 	exports: [RouterModule],
 	declarations: []
 })
---
+++ src/app/components/menu/menu.module.ts
@@ -0,0 +1,17 @@
+import {NgModule, NO_ERRORS_SCHEMA} from '@angular/core';
+import {SharedModule} from '../../shared-module/shared.module';
+import {MenuRoutingModule} from './menu.routing.module';
+import {DashboardComponent} from '../dashboard/dashboard.component';
+
+@NgModule({
+  imports: [
+    SharedModule,
+    MenuRoutingModule
+  ],
+  declarations: [
+    DashboardComponent
+  ],
+  schemas: [NO_ERRORS_SCHEMA]
+})
+
+export class MenuModule {}
