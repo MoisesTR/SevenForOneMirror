fb88f094ffc4912eac2348252dcc83fff466ff1a
Enmanuel <rogeersann97@gmail.com>
2019-02-10 01:20:23 -0600

    add dependence

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,11 +2,36 @@ import { NgModule, Optional, SkipSelf } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { throwIfAlreadyLoaded } from "./module-import-guard";
 import {ReactiveFormsModule} from '@angular/forms';
+import { NG_SELECT_DEFAULT_CONFIG, NgSelectModule } from "@ng-select/ng-select";
+import { DateTimeAdapter, OWL_DATE_TIME_LOCALE, OwlDateTimeModule, OwlNativeDateTimeModule } from "ng-pick-datetime";
+import { NativeDateTimeAdapter } from "ng-pick-datetime/date-time/adapter/native-date-time-adapter.class";
+import { Platform } from "@angular/cdk/platform"; 
 
 @NgModule({
 	imports: [],
-	exports: [CommonModule, ReactiveFormsModule],
-	declarations: []
+	exports: [CommonModule, 
+		ReactiveFormsModule,
+		NgSelectModule,
+		OwlDateTimeModule,
+
+	],
+	declarations: [
+		
+	],
+	providers: [
+		{
+			provide: NG_SELECT_DEFAULT_CONFIG,
+			useValue: {
+				notFoundText: "No se encontraron resultados"
+			}
+		},
+		{ provide: OWL_DATE_TIME_LOCALE, useValue: "es" },
+		{
+			provide: DateTimeAdapter,
+			useClass: NativeDateTimeAdapter,
+			deps: [OWL_DATE_TIME_LOCALE, Platform]
+		}
+	]
 })
 export class CoreModule {
 	constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
