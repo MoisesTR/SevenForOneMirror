deda51e21e8a6aeb1bba0fbe57800c8205ecc186
Enmanuel <rogeersann97@gmail.com>
2019-02-09 23:16:06 -0600

    añadir models

---
+++ src/app/models/User.ts
@@ -0,0 +1,20 @@
+export class User{
+
+    public FirstName: string;
+    public LastName: string;
+    public UserName: string;
+    public Email: string;
+    public Phone: string;
+    public BirthDate: string;
+    public SecretToken: string;
+    public IsVerified: boolean;
+    public IdRole: number;
+    public Password: string
+    public Enable: number;
+    public CreatedAt: string;
+    public UpdateAt: string;
+
+    constructor(){
+
+    }
+}
