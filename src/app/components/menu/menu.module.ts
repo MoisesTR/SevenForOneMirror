03708e7a43c0133e195ab188f9c0b1d9d1a3f114
mtrigueros <moisestrigueros@hotmail.com>
2019-03-10 15:41:18 -0600

    Agregar modulo de mdbangular al menu module

--- src/app/components/menu/menu.module.ts
+++ src/app/components/menu/menu.module.ts
@@ -2,9 +2,11 @@ import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { SharedModule } from "../../shared-module/shared.module";
 import { MenuRoutingModule } from "./menu.routing.module";
 import { DashboardComponent } from "../dashboard/dashboard.component";
+import {MDBBootstrapModulesPro} from 'ng-uikit-pro-standard';
 
 @NgModule({
-	imports: [SharedModule, MenuRoutingModule],
+	imports: [SharedModule, MenuRoutingModule, MDBBootstrapModulesPro],
+  exports: [MDBBootstrapModulesPro],
 	declarations: [DashboardComponent],
 	schemas: [NO_ERRORS_SCHEMA]
 })
