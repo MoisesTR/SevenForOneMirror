52ce936baf6fae3554aa2a785809736f9dae6299
mtrigueros <moisestrigueros@hotmail.com>
2019-03-10 18:48:35 -0600

    Eliminar lineas de codigo innecesarias

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { UsuarioService } from "../../core/services/shared/usuario.service";
 
 import { Token, User } from "../../models/models.index";
-import { ActivatedRoute, Params, Router } from "@angular/router";
-import swal from "sweetalert2";
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
 	selector: "app-dashboard",
@@ -11,10 +10,9 @@ import swal from "sweetalert2";
 	styleUrls: ["./dashboard.component.scss"]
 })
 export class DashboardComponent implements OnInit {
-	public tokenConfirmacion: string;
 	public token: Token;
 	public user: User;
-  public userActual: User;
+	public userActual: User;
 	public usuarios: User[] = [];
 	private username: string;
 	private password: string;
@@ -23,12 +21,6 @@ export class DashboardComponent implements OnInit {
 		this.user = new User();
 		this.token = new Token();
 	}
-
-	elements: any = [
-		{ id: 1, first: "Mark", last: "Otto", handle: "@mdo" },
-		{ id: 2, first: "Jacob", last: "Thornton", handle: "@fat" },
-		{ id: 3, first: "Larry", last: "the Bird", handle: "@twitter" }
-	];
 
 	headElements = ["ID", "First", "Last", "Handle"];
 
@@ -49,47 +41,9 @@ export class DashboardComponent implements OnInit {
 	}
 
 	getParams() {
-		this.activatedRoute.params.subscribe((params: Params) => {
-			this.tokenConfirmacion = params["token"];
-			this.username = localStorage.getItem("username");
-			this.password = localStorage.getItem("password");
-
-			if (params["token"]) {
-				this.verificarUsuario();
-			} else {
-        this.userActual = JSON.parse(localStorage.getItem("identity"));
-				this.getUsuarios();
-			}
-		});
-	}
-
-	verificarUsuario() {
-		this.usuarioService.verifyEmail(this.tokenConfirmacion).subscribe(
-			() => {
-				this.user.userName = this.username;
-				this.user.password = this.password;
-				this.user.getUserInfo = false;
-				this.usuarioService.login(this.user).subscribe(
-					res => {
-						this.token = res;
-						localStorage.setItem("token", this.token.token);
-						this.user.getUserInfo = true;
-						this.usuarioService.login(this.user).subscribe(resuser => {
-							this.usuarioService.identity = resuser;
-							swal.fire("Confirmation", "Welcome to Seven for One, your email is verified!", "success").then(() => {
-                this.userActual = JSON.parse(localStorage.getItem("identity"));
-								this.getUsuarios();
-							});
-						});
-					},
-					() => {
-						this.router.navigate(["/login"]);
-					}
-				);
-			},
-			() => {
-				this.router.navigate(["/login"]);
-			}
-		);
+		this.username = localStorage.getItem("username");
+		this.password = localStorage.getItem("password");
+		this.userActual = JSON.parse(localStorage.getItem("identity"));
+		this.getUsuarios();
 	}
 }
