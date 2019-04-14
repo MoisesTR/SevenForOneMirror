3138cb5f8a610727a1ccc98ce22ad54fddeac809
mtrigueros <moisestrigueros@hotmail.com>
2019-04-14 01:21:20 -0600

    Cambiar estructura de menu principal

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,8 +1,4 @@
 import { Component, OnInit } from "@angular/core";
-import { UsuarioService } from "../../core/services/shared/usuario.service";
-
-import { Token, User } from "../../models/models.index";
-import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
 	selector: "app-dashboard",
@@ -10,56 +6,7 @@ import { ActivatedRoute, Router } from "@angular/router";
 	styleUrls: ["./dashboard.component.scss"]
 })
 export class DashboardComponent implements OnInit {
-	public token: Token;
-	public user: User;
-	public userActual: User;
-	public usuarios: User[] = [];
-	private username: string;
-	private password: string;
+	constructor() {}
 
-	constructor(private activatedRoute: ActivatedRoute, private usuarioService: UsuarioService, private router: Router) {
-		this.user = new User();
-		this.token = new Token();
-	}
-
-	ngOnInit() {
-		this.getParams();
-
-			function fixNavDropdown() {
-					if($( window ).width() <= 575) {
-									$('.navbar .dropdown-menu').removeClass('dropdown-menu-right');
-					} else {
-									$('.navbar .dropdown-menu').addClass('dropdown-menu-right');
-					}
-			}
-
-			$(window).resize(function() {
-					fixNavDropdown();
-			});
-
-			fixNavDropdown();
-	}
-
-	getUsuarios() {
-		this.usuarioService.getUsuarios().subscribe(usuarios => {
-			this.usuarios = usuarios;
-		});
-	}
-
-	logout() {
-		localStorage.clear();
-		this.usuarioService.identity = null;
-		this.router.navigate(["/login"]);
-	}
-
-	getParams() {
-		this.username = localStorage.getItem("username");
-		this.password = localStorage.getItem("password");
-		this.userActual = JSON.parse(localStorage.getItem("identity"));
-		this.getUsuarios();
-	}
-
-	groups() {
-		this.router.navigate(["/groups"]);
-	}
+	ngOnInit() {}
 }
--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { UsuarioService } from "../../core/services/shared/usuario.service";
-import { Router } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
+import { Token, User } from "../../models/models.index";
+declare var $: any;
 
 @Component({
 	selector: "app-menu",
@@ -8,7 +10,56 @@ import { Router } from "@angular/router";
 	styleUrls: ["./menu.component.scss"]
 })
 export class MenuComponent implements OnInit {
-	constructor(private router: Router, private usuarioService: UsuarioService) {}
+	public token: Token;
+	public user: User;
+	public userActual: User;
+	public usuarios: User[] = [];
+	private username: string;
+	private password: string;
 
-	ngOnInit() {}
+	constructor(private activatedRoute: ActivatedRoute, private usuarioService: UsuarioService, private router: Router) {
+		this.user = new User();
+		this.token = new Token();
+	}
+
+	ngOnInit() {
+		this.getParams();
+
+		function fixNavDropdown() {
+			if ($(window).width() <= 575) {
+				$(".navbar .dropdown-menu").removeClass("dropdown-menu-right");
+			} else {
+				$(".navbar .dropdown-menu").addClass("dropdown-menu-right");
+			}
+		}
+
+		$(window).resize(function() {
+			fixNavDropdown();
+		});
+
+		fixNavDropdown();
+	}
+
+	getUsuarios() {
+		this.usuarioService.getUsuarios().subscribe(usuarios => {
+			this.usuarios = usuarios;
+		});
+	}
+
+	getParams() {
+		this.username = localStorage.getItem("username");
+		this.password = localStorage.getItem("password");
+		this.userActual = JSON.parse(localStorage.getItem("identity"));
+		this.getUsuarios();
+	}
+
+	groups() {
+		this.router.navigate(["/groups"]);
+	}
+
+	logout() {
+		localStorage.clear();
+		this.usuarioService.identity = null;
+		this.router.navigate(["/login"]);
+	}
 }
