568f2683840a8f94592c86b45d2ad7fdc16aa261
mtrigueros <moisestrigueros@hotmail.com>
2019-04-15 19:20:36 -0600

    Incorporar metodos a servicios de group,rol y usuario

--- src/app/core/services/shared/rol.service.ts
+++ src/app/core/services/shared/rol.service.ts
@@ -1,9 +1,31 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { Global } from "./global";
+import { Observable } from "rxjs";
+import { HttpClient } from "@angular/common/http";
+import { Role } from "../../../models/Role";
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: "root"
 })
 export class RolService {
+	public url: string;
+	public urlRol = "roles";
 
-  constructor() { }
+	constructor(private http: HttpClient) {
+		this.url = Global.urlAuth;
+	}
+
+	getRol(rolId): Observable<any> {
+		return this.http.get(this.url + this.urlRol + "/" + rolId);
+	}
+
+	getRoles(): Observable<any> {
+		return this.http.get(this.url + this.urlRol);
+	}
+
+	filterIdRolUser(roles: Role[]) {
+		// Rol desginado para los usuarios normales
+		const idRol = roles.find(rol => rol.name === "User")._id;
+		return idRol ? idRol : '0';
+	}
 }
--- src/app/core/services/shared/usuario.service.ts
+++ src/app/core/services/shared/usuario.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { Global } from "./global";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { User } from "../../../models/models.index";
+import { User } from '../../../models/User';
 import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
 
@@ -9,9 +9,10 @@ import { map } from "rxjs/operators";
 export class UsuarioService {
 	public url: string;
 	public identity: User;
+	public userUrl = "users";
 
 	constructor(private http: HttpClient) {
-		this.url = Global.url;
+		this.url = Global.urlAuth;
 	}
 
 	login(usuario: User): Observable<any> {
@@ -23,17 +24,17 @@ export class UsuarioService {
 		return this.http.post(this.url + "login", usuario, options);
 	}
 
-	loginGoogle(usuario: User) : Observable<any> {
-    const headers = new HttpHeaders({
-      "Content-Type": "application/json"
-    });
-    const options = { headers: headers };
+	loginGoogle(usuario: User): Observable<any> {
+		const headers = new HttpHeaders({
+			"Content-Type": "application/json"
+		});
+		const options = { headers: headers };
 
-    return this.http.post(this.url + "loginGoogle", usuario, options);
-  }
+		return this.http.post(this.url + "loginGoogle", usuario, options);
+	}
 
 	getUsuarios() {
-		return this.http.get<User[]>(this.url + "users").pipe(map(data => data));
+		return this.http.get<User[]>(this.url + this.userUrl).pipe(map(data => data));
 	}
 
 	createUsuario(usuario: User) {
@@ -43,6 +44,15 @@ export class UsuarioService {
 		const options = { headers: headers };
 
 		return this.http.post(this.url + "signup", usuario, options);
+	}
+
+	updateUser(usuario: User): Observable<any> {
+		const headers = new HttpHeaders({
+			"Content-Type": "application/json"
+		});
+		const options = { headers: headers };
+
+		return this.http.put(this.url + this.userUrl, usuario);
 	}
 
 	verifyEmail(token) {
