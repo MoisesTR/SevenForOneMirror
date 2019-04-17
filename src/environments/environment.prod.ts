bde4e90759b6571310f235a2c2e55040cc51fa31
mtrigueros <moisestrigueros@hotmail.com>
2019-04-16 23:15:54 -0600

    Corregir url de produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiEndpoint: 'http://ec2-34-234-73-127.compute-1.amazonaws.com.com:3000/',
+  apiEndpoint: 'http://ec2-34-234-73-127.compute-1.amazonaws.com:3000/',
   apiEndpointAuth: 'http://ec2-34-234-73-127.compute-1.amazonaws.com:3000/auth/'
 };
