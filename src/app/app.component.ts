72025e88868eb091acf680e2f5525e9896249ddb
Enmanuel <rogeersann97@gmail.com>
2019-04-26 18:25:59 -0600

    añadir caracteristica SEO

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,38 +7,33 @@ import { Meta, Title } from '@angular/platform-browser'
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
-
+  nombreApp: string = 'SevenForOne'
+  title: string = 'SevenForOne: Ganar Dinero Facil y Seguro';
+  descripcion = 'Ganar dinero extra online de manera facil, en esta pagina web podras duplicar tu dinero con SevenForOne ganas al instante invita a tus amigos para que las oportunidad de ganar sean mayores';
+  urlsitio: string = ''
   constructor(seo: Meta, title: Title) {
-
-    // title.setTitle('Probando el seo');
-    seo.addTags([
-      { name: 'author', content: 'Moises Israel' },
-      { name: 'keywords', content: 'Money, mucho dinero, Dinero, billetes' },
-      { name: 'description', content: 'con este app ganaras dinero a lo grande, asi que descargala' }
-    ]);
 
     //twitter meta datos
     seo.addTags([
       { name: 'twitter:card', content: 'summary' },
-      { name: 'twitter:title', content: 'Probando el titulo' },
-      { name: 'twitter:description', content: 'Probando la descripcion de esta app' },
-      { name: 'twitter:creator', content: '@pruebacompartir' },
+      { name: 'twitter:title', content: this.title },
+      { name: 'twitter:description', content: this.descripcion },
+      { name: 'twitter:creator', content: this.nombreApp },
       { name: 'twitter:image', content: 'https://avatars3.githubusercontent.com/u/16628445?v=3&s=200' }
     ]);
 
     //open Graph data for facebook
 
     seo.addTags([
-      { name: ' og:title', content: 'Titulo en Face' },
-      { name: 'og:type', content: 'article' },
-      { name: 'og:description', content: 'Descripcion facebook aqui' },
-      { name: 'og:image', content: 'https://avatars3.githubusercontent.com/u/16628445?v=3&s=200' }
-
+      { property: 'og:title', content: this.title },
+      { property: 'og:type', content: 'article' },
+      { property: 'og:description', content: this.descripcion },
+      { property: 'og:url', content: this.urlsitio },
+      { property: 'og:site_name', content: this.nombreApp },
+      { property: 'og:image', content: 'https://avatars3.githubusercontent.com/u/16628445?v=3&s=200' },
+      { property: 'article:section', content: 'SevenForOne Ganar Dinero' },
+      { property: 'article:tag', contetn: 'sevenforone' },
     ]);
-
-    //google +
-
   }
 
 }
