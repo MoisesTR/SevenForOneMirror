9ec9173ef7433084ab443358ae04e60b12888a6d
mtrigueros <moisestrigueros@hotmail.com>
2019-06-15 18:37:28 -0600

    Test url api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiEndpoint: 'http://ec2-35-162-173-135.us-west-2.compute.amazonaws.com:3000/',
-  apiEndpointAuth: 'http://ec2-35-162-173-135.us-west-2.compute.amazonaws.com:3000/auth/'
+  apiEndpoint: 'http://ec2-35-162-173-135.us-west-2.compute.amazonaws.com:3000/api',
+  apiEndpointAuth: 'http://ec2-35-162-173-135.us-west-2.compute.amazonaws.com:3000/api/auth/'
 };
