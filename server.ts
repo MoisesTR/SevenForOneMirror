71ebacaee3c65cbd45a553e67599d2f2bd60c7ec
mtrigueros <moisestrigueros@hotmail.com>
2019-06-19 22:47:30 -0600

    Agregar variable global windows en el server.ts

--- server.ts
+++ server.ts
@@ -1,12 +1,17 @@
 import 'zone.js/dist/zone-node';
+import 'reflect-metadata';
 import {enableProdMode} from '@angular/core';
-// Express Engine
-import {ngExpressEngine} from '@nguniversal/express-engine';
-// Import module map for lazy loading
-import {provideModuleMap} from '@nguniversal/module-map-ngfactory-loader';
 
 import * as express from 'express';
 import {join} from 'path';
+import {readFileSync} from 'fs';
+// Express Engine
+import {renderModuleFactory} from '@angular/platform-server';
+// Import module map for lazy loading
+
+const domino = require('domino');
+const fs = require('fs');
+const path = require('path');
 
 // Faster server renders w/ Prod mode (dev mode never needed)
 enableProdMode();
@@ -17,16 +22,31 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 const DIST_FOLDER = join(process.cwd(), 'dist/browser');
 
+// Our index.html we'll use as our template
+const template = readFileSync(join(DIST_FOLDER, 'index.html')).toString();
+const win = domino.createWindow(template);
+global['window'] = win;
+global['document'] = win.document;
+
 // * NOTE :: leave this as require() since this file is built Dynamically from webpack
-const {AppServerModuleNgFactory, LAZY_MODULE_MAP} = require('./dist/server/main');
+const { AppServerModuleNgFactory, LAZY_MODULE_MAP } = require('./dist/server/main');
+
+const { provideModuleMap } = require('@nguniversal/module-map-ngfactory-loader');
 
 // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
-app.engine('html', ngExpressEngine({
-  bootstrap: AppServerModuleNgFactory,
-  providers: [
-    provideModuleMap(LAZY_MODULE_MAP)
-  ]
-}));
+app.engine('html', (_, options, callback) => {
+  renderModuleFactory(AppServerModuleNgFactory, {
+    // Our index.html
+    document: template,
+    url: options.req.url,
+    // DI so that we can get lazy-loading to work differently (since we need it to just instantly render it)
+    extraProviders: [
+      provideModuleMap(LAZY_MODULE_MAP)
+    ]
+  }).then(html => {
+    callback(null, html);
+  });
+});
 
 app.set('view engine', 'html');
 app.set('views', DIST_FOLDER);
