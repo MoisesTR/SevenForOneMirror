c22c0b87a6939c679c99b8cf8083302b5d7c8282
mtrigueros <moisestrigueros@hotmail.com>
2019-07-06 12:14:45 -0600

    Agregar parametros extras a la conexion del socket game

--- src/app/core/services/shared/socket-group-game.service.ts
+++ src/app/core/services/shared/socket-group-game.service.ts
@@ -4,7 +4,8 @@ import { environment } from "../../../../environments/environment";
 import { EventEnum } from "../../../enums/EventEnum";
 import { Observable } from "rxjs";
 import { NameSpaceEnum } from "../../../enums/NameSpaceEnum";
-import {NGXLogger} from "ngx-logger";
+import { NGXLogger } from "ngx-logger";
+import { AuthService } from "../auth/auth.service";
 
 @Injectable({
 	providedIn: "root"
@@ -12,47 +13,45 @@ import {NGXLogger} from "ngx-logger";
 export class SocketGroupGameService {
 	private socket;
 
-	constructor(private logger: NGXLogger) {}
+	constructor(private logger: NGXLogger, private authService: AuthService) {}
 
 	public connect() {
-		this.socket = socketIo.connect(environment.socket + "/" + NameSpaceEnum.groupGame);
+		const userName = this.authService.getUser().userName;
+		this.logger.info('CONNECT TO TO SOCKET GAME');
+		this.socket = socketIo.connect(environment.socket + "/" + NameSpaceEnum.groupGame, { query: { userName } });
 
-		this.onEvent(EventEnum.CONNECT).subscribe(() => {
-
-		});
-		this.onEvent(EventEnum.DISCONNECT).subscribe(() => {
-
-		});
+		this.onEvent(EventEnum.CONNECT).subscribe(() => {});
+		this.onEvent(EventEnum.DISCONNECT).subscribe(() => {});
 	}
 
 	public send(event: EventEnum, payload: string): void {
-    this.logger.info('GAME SOCKET EVENT EMIT: ' + event);
+		this.logger.info("GAME SOCKET EVENT EMIT: " + event);
 		this.socket.emit(event, payload);
 	}
 
 	public onEvent(event: EventEnum): Observable<any> {
-    this.logger.info('GAME SOCKET EVENT ON: ' + event);
+		this.logger.info("GAME SOCKET EVENT ON: " + event);
 		return new Observable<EventEnum>(observer => {
 			this.socket.on(event, data => observer.next(data));
 		});
 	}
 
 	public onEventGroup(event: string): Observable<any> {
-    this.logger.info('GAME SOCKET EVENT ON: ' + event);
+		this.logger.info("GAME SOCKET EVENT ON: " + event);
 		return new Observable<String>(observer => {
 			this.socket.on(event, data => observer.next(data));
 		});
 	}
 
-  public closeSocket() {
-    if (this.socket) {
-      this.logger.info('CLOSE MAIN SOCKET');
-      this.socket.close();
-    }
-  }
+	public closeSocket() {
+		if (this.socket) {
+			this.logger.info("CLOSE MAIN SOCKET");
+			this.socket.close();
+		}
+	}
 
-  public removeAllListeners() {
-	  this.logger.info('Remove all listeners game socket');
-    this.socket.removeAllListeners();
-  }
+	public removeAllListeners() {
+		this.logger.info("Remove all listeners game socket");
+		this.socket.removeAllListeners();
+	}
 }
