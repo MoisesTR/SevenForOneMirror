ade41b490da5971eb33f98a864ab4fa7e279a8fb
JackZeled0n <jackiezeledon@gmail.com>
2019-07-06 22:32:32 -0600

    Incorporar top players

--- src/app/components/menu/menu.routing.module.ts
+++ src/app/components/menu/menu.routing.module.ts
@@ -5,6 +5,7 @@ import {ProfileComponent} from '../profile/profile.component';
 import {GroupsComponent} from '../groups/groups.component';
 import {GamecontainerComponent} from '../gamecontainer/gamecontainer.component';
 import {AddGroupsComponent} from '../add-groups/add-groups.component';
+import { TopPlayersComponent } from '../top-players/top-players.component';
 
 const menuRoutes = [
 	{ path: "dashboard", component: DashboardComponent, data: { titulo: "Dashboard" } }
@@ -13,6 +14,7 @@ const menuRoutes = [
 	, { path: "", redirectTo: "/dashboard", pathMatch: "full", data: { titulo: "Dashboard" } }
 	, { path: "game/:idGroup", component: GamecontainerComponent, data: { titulo: "Game container"}}
 	, { path: "add-group", component: AddGroupsComponent, data: { titulo: "Add Group" } }
+	, { path: "top-players", component: TopPlayersComponent, data: { titulo: "Top Players" } }
 ];
 
 @NgModule({
