4640f664cda5fa9c9a4f15fb302fab959a5cdb48
mtrigueros <moisestrigueros@hotmail.com>
2019-07-09 19:28:36 -0600

    Actualizar ID de paypal client

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,9 @@
 export const environment = {
   production: true,
   apiEndpoint: 'https://www.antaresyaldebaran.me/api/',
-  socket: 'http://localhost:3000',
+  socket: 'https://www.antaresyaldebaran.me',
   apiEndpointAuth: 'https://www.antaresyaldebaran.me/api/auth/',
-  paypalClienttId: 'AesFU7Gm3IQFQTtJ_T9KW_a6DQIDebT7FBXizSgYhRaeiHxfyv6WoENa1dcbthiFyidViSkzevyTuauh',
+  // paypalClienttId: 'AesFU7Gm3IQFQTtJ_T9KW_a6DQIDebT7FBXizSgYhRaeiHxfyv6WoENa1dcbthiFyidViSkzevyTuauh',
+  paypalClienttId: 'AeAOXVYNJiULSY8n52TeZONxNOjl6vRT5qA9iGFbUUy_0l-mtBS_rDFc3jyJajkhKykw8YM4VZAY9KFw',
   googleClientId: '380320064033-bs2uivmdsj2fs5v68h2kg57p5k9kgtv7.apps.googleusercontent.com'
 };
