2b4284282038d6a348084285b9881973919d00bd
mtrigueros <moisestrigueros@hotmail.com>
2019-08-08 23:41:03 -0600

    Fix temporal bug modal celebration

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -248,7 +248,7 @@ export class MenuComponent implements OnInit, OnDestroy {
 	}
 
 	clainEvent() {
-		this.modalWin.hide();
+		// this.modalWin.hide();
 		this.router.navigateByUrl("/win-history");
 	}
 }
