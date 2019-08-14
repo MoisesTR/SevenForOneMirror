088d6df66efbf97717b6130e899910727860430a
mtrigueros <moisestrigueros@hotmail.com>
2019-08-13 21:45:27 -0600

    Rename shared component showError and directives

--- src/app/shared-module/shared.module.ts
+++ src/app/shared-module/shared.module.ts
@@ -2,29 +2,28 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { DirectivesModule } from "../directives/directives.module";
-import { ShowErrorsComponent } from "../components/show-errors.component";
 import { DateTimeAdapter, OWL_DATE_TIME_LOCALE, OwlDateTimeModule, OwlNativeDateTimeModule } from "ng-pick-datetime";
 import { NativeDateTimeAdapter } from "ng-pick-datetime/date-time/adapter/native-date-time-adapter.class";
 import { Platform } from "@angular/cdk/platform";
 import { NgxSpinnerModule } from "ngx-spinner";
 import { ImageUserPipe } from "../pipe/image-user.pipe";
-import {EmptyRecordsComponent} from "../components/empty-records/empty-records.component";
-import {ModalModule} from "ng-uikit-pro-standard";
+import { EmptyRecordsComponent } from "../components/empty-records/empty-records.component";
+import { ShowErrorsComponent } from "../components/show-errors.component";
 
 @NgModule({
 	imports: [CommonModule],
-  exports: [
-    CommonModule,
-    ReactiveFormsModule,
-    FormsModule,
-    DirectivesModule,
-    ShowErrorsComponent,
-    OwlDateTimeModule,
-    OwlNativeDateTimeModule,
-    NgxSpinnerModule,
-    ImageUserPipe,
-    EmptyRecordsComponent
-  ],
+	exports: [
+		CommonModule,
+		ReactiveFormsModule,
+		FormsModule,
+		DirectivesModule,
+		ShowErrorsComponent,
+		OwlDateTimeModule,
+		OwlNativeDateTimeModule,
+		NgxSpinnerModule,
+		ImageUserPipe,
+		EmptyRecordsComponent
+	],
 	declarations: [ShowErrorsComponent, ImageUserPipe, EmptyRecordsComponent],
 	providers: [
 		{ provide: OWL_DATE_TIME_LOCALE, useValue: "es" },
