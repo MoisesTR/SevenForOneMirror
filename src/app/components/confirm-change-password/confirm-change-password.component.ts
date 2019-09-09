02fc7bbd7c4126402bdefb281777a8f3ac3bc0a9
JackZeled0n <jackiezeledon@gmail.com>
2019-09-08 23:10:06 -0600

    Add new component for confirming change password

---
+++ src/app/components/confirm-change-password/confirm-change-password.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+	selector: "app-confirm-change-password",
+	templateUrl: "./confirm-change-password.component.html",
+	styleUrls: ["./confirm-change-password.component.scss"]
+})
+export class ConfirmChangePasswordComponent implements OnInit {
+	constructor() {}
+
+	ngOnInit() {}
+}
