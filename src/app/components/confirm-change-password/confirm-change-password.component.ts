b726b58efdae3e015b08f1081d81df2b709fc7d6
JackZeled0n <jackiezeledon@gmail.com>
2019-09-09 23:08:13 -0600

    Add body and style of confirm-change-password component

--- src/app/components/confirm-change-password/confirm-change-password.component.ts
+++ src/app/components/confirm-change-password/confirm-change-password.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormControl, FormGroup } from "@angular/forms";
 
 @Component({
 	selector: "app-confirm-change-password",
@@ -6,7 +7,33 @@ import { Component, OnInit } from "@angular/core";
 	styleUrls: ["./confirm-change-password.component.scss"]
 })
 export class ConfirmChangePasswordComponent implements OnInit {
-	constructor() {}
+	formChangePassword: FormGroup;
 
-	ngOnInit() {}
+	constructor(private formBuilder: FormBuilder) {}
+
+	ngOnInit() {
+		this.initFormGroup();
+	}
+
+	initFormGroup() {
+		this.formChangePassword = this.formBuilder.group({
+			initialInvertion: new FormControl(0)
+		});
+	}
+
+	showPassword(idTextField) {
+		const inputType = (<HTMLInputElement>document.getElementById(idTextField)).type;
+		const iconEyeOpen = document.getElementById("eye-open");
+		const iconEyeLinked = document.getElementById("eye-linked");
+
+		if (inputType === "password") {
+			document.getElementById(idTextField).setAttribute("type", "text");
+			iconEyeOpen.style.display = "none";
+			iconEyeLinked.style.display = "block";
+		} else {
+			document.getElementById(idTextField).setAttribute("type", "password");
+			iconEyeOpen.style.display = "block";
+			iconEyeLinked.style.display = "none";
+		}
+	}
 }
