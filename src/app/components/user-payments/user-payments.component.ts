51c053d5c82dc9322a33fec3d20659fad3c6ca1a
JackZeled0n <jackiezeledon@gmail.com>
2019-09-20 00:08:01 -0600

    Add table estructure in user-payment component

--- src/app/components/user-payments/user-payments.component.ts
+++ src/app/components/user-payments/user-payments.component.ts
@@ -1,12 +1,50 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ElementRef, ViewChild, AfterViewInit, ChangeDetectorRef } from "@angular/core";
+import { MdbTableDirective, MdbTablePaginationComponent } from "ng-uikit-pro-standard";
 
 @Component({
 	selector: "app-user-payments",
 	templateUrl: "./user-payments.component.html",
 	styleUrls: ["./user-payments.component.scss"]
 })
-export class UserPaymentsComponent implements OnInit {
-	constructor() {}
+export class UserPaymentsComponent implements OnInit, AfterViewInit {
+	// DATATABLES PROPERTIES
+	@ViewChild(MdbTableDirective) mdbTable: MdbTableDirective;
+	@ViewChild(MdbTablePaginationComponent) mdbTablePagination: MdbTablePaginationComponent;
+	@ViewChild("row") row: ElementRef;
 
-	ngOnInit() {}
+	elements: any = [];
+	headElements = ["#", "Usuario", "Nombres", "Correo de Pago", "Grupo", "Ganancia", "Fecha", "Acción"];
+
+	previous: string;
+
+	maxVisibleItems = 15;
+
+	constructor(private cdRef: ChangeDetectorRef) {}
+
+	ngOnInit() {
+		for (let i = 1; i <= 15; i++) {
+			this.elements.push({
+				id: i.toString(),
+				user: "UsuarioTest" + i,
+				names: "Nombres " + i,
+				email: "testemail@7x1.com",
+				group: i.toString() + "0",
+				earned: i.toString() + "00",
+				date: "19/09/2019",
+				buttons: ""
+			});
+		}
+
+		this.mdbTable.setDataSource(this.elements);
+		this.elements = this.mdbTable.getDataSource();
+		this.previous = this.mdbTable.getDataSource();
+	}
+
+	ngAfterViewInit() {
+		this.mdbTablePagination.setMaxVisibleItemsNumberTo(this.maxVisibleItems);
+
+		this.mdbTablePagination.calculateFirstItemIndex();
+		this.mdbTablePagination.calculateLastItemIndex();
+		this.cdRef.detectChanges();
+	}
 }
